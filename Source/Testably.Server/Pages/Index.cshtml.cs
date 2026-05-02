4276db58bb9a0ff5510b301c517674cc7c3c5376
Valentin Breuß <vbreuss@gmail.com>
2026-05-02 17:58:57 +0200

    feat: redirect index page to documentation (#16)
    This pull request updates the `Index.cshtml.cs` page model to redirect users to external documentation instead of rendering the default page. The change also removes unnecessary logging code.

--- Source/Testably.Server/Pages/Index.cshtml.cs
+++ Source/Testably.Server/Pages/Index.cshtml.cs
@@ -1,18 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Testably.Server.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-    }
+    public IActionResult OnGet()
+        => RedirectPermanent("https://docs.testably.org");
 }
